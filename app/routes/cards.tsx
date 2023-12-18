5f4e4a883be0973879a7e55dffc3cf619e6e2169
Jordy <jordynavalee@gmail.com>
2023-12-18 10:13:25 -0600

    adds edit/delete buttons for individual cards in all cards view"

--- app/routes/cards.tsx
+++ app/routes/cards.tsx
@@ -1,7 +1,8 @@
 import { LoaderFunctionArgs, json } from "@remix-run/node";
-import { useLoaderData, Outlet } from "@remix-run/react";
+import { useLoaderData, Outlet, Form } from "@remix-run/react";
 import { cards } from "db/schema";
 import { drizzle } from "~/utils/db.server";
+import { Link } from "@remix-run/react";
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const allCards = await drizzle.select().from(cards);
@@ -19,8 +20,26 @@ export default function Cards() {
       <h1>Cards</h1>
       <Outlet />
       {cardsArray.map((card) => (
-        <div key={card.value.id}>
-          {card.value.front} | {card.value.back}
+        <div className="card-container" key={card.value.id}>
+          {card.value.front} ||| {card.value.back}
+          <div className="button-container">
+            <Link className="button" to={`/cards/${card.value.id}/edit`}>
+              Edit
+            </Link>
+            <Form
+              method="post"
+              onSubmit={(event) => {
+                const response = confirm(
+                  "Please confirm you want to delete this record."
+                );
+                if (!response) {
+                  event.preventDefault();
+                }
+              }}
+            >
+              <button type="submit">Delete</button>
+            </Form>
+          </div>
         </div>
       ))}
     </>
