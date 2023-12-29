11f5003298f84c4e6f250400ce9b7918898a1557
Jordy <jordynavalee@gmail.com>
2023-12-29 07:42:37 -0600

    redirects users to all decks when logging in

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,3 +1,15 @@
+import { LoaderFunctionArgs, redirect } from "@remix-run/node";
+import { authCookie } from "~/auth";
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  let cookieString = request.headers.get("Cookie");
+  let userId = await authCookie.parse(cookieString);
+  if (userId) {
+    throw redirect("/decks");
+  }
+  return null;
+}
+
 export default function Index() {
   return (
     <p id="index-page">
