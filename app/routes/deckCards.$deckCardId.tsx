ac5b1ae647d3bf8233794594f8117fd84ba69d14
Jordy <jordynavalee@gmail.com>
2024-01-05 10:38:10 -0600

    repairs single deckCard view

--- app/routes/deckCards.$deckCardId.tsx
+++ app/routes/deckCards.$deckCardId.tsx
@@ -1,57 +1,80 @@
 import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
 import { eq } from "drizzle-orm";
-import { Form, useLoaderData, Link } from "@remix-run/react";
+import {
+  Form,
+  useLoaderData,
+  Link,
+  useFetcher,
+  Outlet,
+} from "@remix-run/react";
 import React from "react";
 
 import { drizzle } from "../utils/db.server";
-import { deckCards } from "../../db/schema";
+import { deckCards, decks, cards } from "../../db/schema";
 import { z } from "zod";
 
-export const loader = async ({ params }: LoaderFunctionArgs) => {
-  const allDeckCards = await drizzle.select().from(deckCards);
-
-  if (!allDeckCards || allDeckCards.length === 0) {
-    throw new Response("No cards in this deck");
+export async function loader({ request, params }: LoaderFunctionArgs) {
+  try {
+    const singleDeckCard = await drizzle
+      .select()
+      .from(deckCards)
+      .innerJoin(decks, eq(deckCards.deckID, decks.id))
+      .innerJoin(cards, eq(deckCards.cardID, cards.id))
+      .where(eq(deckCards.id, Number(params.deckCardId)));
+    return json(singleDeckCard);
+  } catch (error) {
+    console.error("Loader error:", error);
+    throw new Response("Error loading deck cards", { status: 500 });
   }
+}
 
-  const name = deckCards[0]?.name;
-
-  return json({ allDeckCards, name });
-};
+const deckCardIdSchema = z.object({
+  deckCardId: z.string(),
+});
 
 export const action = async ({ params }: ActionFunctionArgs) => {
-  const deckCardId = z.coerce.number().parse(params.deckCardId);
+  const parsedDeckCardId = deckCardIdSchema.safeParse(params.deckCardId);
+
+  console.log({ cardz_delete_error: params.error });
+
+  if (!parsedDeckCardId.success) {
+    return json({ error: "No deck card id provided" }, { status: 400 });
+  }
 
   try {
-    await drizzle.delete(deckCards).where(eq(deckCards.id, deckCardId));
+    await drizzle
+      .delete(deckCards)
+      .where(eq(deckCards.cardID, Number(parsedDeckCardId)));
     return redirect(`/deckCards`);
   } catch (error) {
-    console.log({ deck_delete_error: error });
     return json({ status: "error" });
   }
 };
 
-export default function Deck({}) {
-  const { allDeckCards } = useLoaderData<typeof loader>();
-  const deckData = allDeckCards[0];
+export default function DeckCards() {
+  const singleDeckCard = useLoaderData<typeof loader>();
+  const fetcher = useFetcher();
 
-  if (!deckData) {
-    return <div>Deck not found.</div>;
-  }
-  if (!allDeckCards) {
-    return <div>No cards in this deck.</div>;
+  if (!singleDeckCard || singleDeckCard.length === 0) {
+    return <div>Decks not found.</div>;
   }
 
+  console.log({ singleDeckCard });
   return (
     <div id="deck">
-      <h1>{deckData.id}</h1>
+      <h1>Single Card View</h1>
       <div className="single-card-container">
-        {allDeckCards.map((card) => {
+        {singleDeckCard.map((card) => {
           return (
-            <div key={card.id} className="card-box">
-              <div className="single-card"></div>
-              <Link to={`/deckCards/${deckData.id}/edit`}>Edit</Link>
+            <div key={card.cards.id} className="card-box">
+              <div className="single-card">
+                <h2>{card.cards.front}</h2>
+                <h2>{card.cards.back}</h2>
+                <h2>{card.cards.CEFR_level}</h2>
+                <h2>{card.cards.frequency}</h2>
+              </div>
+              {/* <Link to={`/decks/${singleDeckCard.id}/edit`}>Edit</Link> */}
               <Form
                 method="post"
                 onSubmit={(event) => {
