0a2db672a6848aa6f25fd279a074e7d8db4256da
Jordy <jordynavalee@gmail.com>
2024-02-02 10:02:25 -0600

    removes subscribe action from my decks into specific deck

--- app/routes/decks.$deckId.tsx
+++ app/routes/decks.$deckId.tsx
@@ -1,17 +1,31 @@
 import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
-import { eq, orderBy } from "drizzle-orm";
+import { eq, asc, and, orderBy } from "drizzle-orm";
 import { Form, useLoaderData, Link } from "@remix-run/react";
 import React from "react";
 
 import { drizzle } from "../utils/db.server";
-import { decks, deckCards, cards, userCards } from "../../db/schema";
+import {
+  decks,
+  deckCards,
+  cards,
+  userCards,
+  userDeckSubscriptions,
+} from "../../db/schema";
 import { z } from "zod";
 import BorderColorRoundedIcon from "@mui/icons-material/BorderColorRounded";
 import DeleteRoundedIcon from "@mui/icons-material/DeleteRounded";
 import { useFetcher, Outlet } from "@remix-run/react";
+import { getAuthCookie, requireAuthCookie } from "../auth";
 
-export const loader = async ({ params }: LoaderFunctionArgs) => {
+export const loader = async ({ params, request }: LoaderFunctionArgs) => {
+  const deckId = z.coerce.number().parse(params.deckId);
+
+  if (!deckId) {
+    throw new Error("Invalid deck ID");
+  }
+  const userId = await getAuthCookie(request);
+
   const deck = await drizzle
     .select()
     .from(decks)
@@ -30,6 +44,21 @@ export const loader = async ({ params }: LoaderFunctionArgs) => {
     .leftJoin(userCards, eq(cards.id, userCards.cardID))
     .where(eq(deckCards.deckID, Number(params.deckId)))
     .orderBy(deckCards.id);
+
+  if (!userId) {
+    return json({
+      deckId,
+      deck,
+      allDeckCards,
+      deckCardArr,
+      isAuth: false,
+      userSubscriptions: null,
+    } as const);
+  }
+  const userSubscriptions = await drizzle
+    .select()
+    .from(userDeckSubscriptions)
+    .where(eq(userDeckSubscriptions.userID, userId));
 
   // do this in loader!
 
@@ -51,15 +80,69 @@ export const loader = async ({ params }: LoaderFunctionArgs) => {
 
   const name = deck[0]?.name;
 
-  return json({ deck, allDeckCards, name, deckCardArr });
+  return json({
+    deckId,
+    deck,
+    allDeckCards,
+    name,
+    deckCardArr,
+    userSubscriptions,
+  });
 };
 
-export const action = async ({ params }: ActionFunctionArgs) => {
+export const action = async ({ params, request }: ActionFunctionArgs) => {
+  const userId = await requireAuthCookie(request);
   const deckId = z.coerce.number().parse(params.deckId);
+  const formData = await request.formData();
+  const subscribeString = formData.get("subscribe");
+
+  const isSubscribeAction = formData.has("subscribe");
+
+  const firstCard = await drizzle
+    .select({ cardID: deckCards.cardID })
+    .from(deckCards)
+    .where(eq(deckCards.deckID, deckId))
+    .orderBy(asc(deckCards.cardID))
+    .limit(1);
 
   try {
-    await drizzle.delete(decks).where(eq(decks.id, deckId));
-    return redirect(`/decks`);
+    if (isSubscribeAction) {
+      const subscribe = Boolean(z.coerce.number().parse(subscribeString));
+      const [existingSubscription] = await drizzle
+        .select()
+        .from(userDeckSubscriptions)
+        .where(
+          and(
+            eq(userDeckSubscriptions.deckID, deckId),
+            eq(userDeckSubscriptions.userID, userId)
+          )
+        )
+        .limit(1);
+
+      if (firstCard[0]?.cardID) {
+        await drizzle
+          .insert(userCards)
+          .values({ userID: userId, cardID: firstCard[0]?.cardID })
+          .onConflictDoNothing({
+            target: [userCards.userID, userCards.cardID],
+          });
+      }
+
+      if (existingSubscription) {
+        await drizzle
+          .update(userDeckSubscriptions)
+          .set({ subscribed: subscribe })
+          .where(eq(userDeckSubscriptions.id, existingSubscription.id));
+      } else {
+        await drizzle
+          .insert(userDeckSubscriptions)
+          .values({ userID: userId, deckID: deckId, subscribed: subscribe });
+      }
+      return null;
+    } else if (!isSubscribeAction) {
+      await drizzle.delete(decks).where(eq(decks.id, deckId));
+      return redirect(`/decks`);
+    }
   } catch (error) {
     console.log({ deck_delete_error: error });
     return json({ status: "error" });
@@ -67,9 +150,17 @@ export const action = async ({ params }: ActionFunctionArgs) => {
 };
 
 export default function Deck({}) {
-  const { deck, deckCardArr } = useLoaderData<typeof loader>();
+  const { deckId, deck, deckCardArr, userSubscriptions } =
+    useLoaderData<typeof loader>();
   const deckData = deck[0];
   const fetcher = useFetcher();
+
+  const isSubscribed =
+    Number(fetcher.formData?.get("deckId")) === deckId
+      ? Boolean(fetcher.formData?.get("subscribe"))
+      : userSubscriptions?.find(
+          (subscription) => subscription.deckID === deckId
+        )?.subscribed;
 
   if (!deckData) {
     return <div>Deck not found.</div>;
@@ -90,7 +181,19 @@ export default function Deck({}) {
       {/* <p>
         {`This deck has ${thisDeckCard.length} cards. You have reviewed ${numReviewed}/${thisDeckCard.length} cards.`}
       </p> */}
+
       <div className="deck-setting-section">
+        <fetcher.Form method="POST">
+          <button
+            aria-label="Toggle Subscription"
+            className={isSubscribed ? "unsubscribe-button" : "subscribe-button"}
+            name="subscribe"
+            value={isSubscribed ? 0 : 1}
+          >
+            {isSubscribed ? "Unsubscribe" : "Subscribe"}
+          </button>
+        </fetcher.Form>
+
         <fetcher.Form
           method="post"
           action={`/decks/${deckData.id}/delete`}
--- app/routes/decks._index.tsx
+++ app/routes/decks._index.tsx
@@ -1,22 +1,10 @@
-import {
-  LoaderFunctionArgs,
-  ActionFunctionArgs,
-  json,
-  redirect,
-} from "@remix-run/node";
-import { useLoaderData, Outlet, useActionData } from "@remix-run/react";
-import {
-  decks,
-  userDeckSubscriptions,
-  deckCards,
-  userCards,
-} from "../../db/schema";
-import { z } from "zod";
-import { eq, and, asc } from "drizzle-orm";
+import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { decks, userDeckSubscriptions, deckCards } from "../../db/schema";
+import { eq } from "drizzle-orm";
 import { drizzle } from "../utils/db.server";
 import { Link, Form } from "@remix-run/react";
-import { useFetcher } from "@remix-run/react";
-import { getAuthCookie, requireAuthCookie } from "../auth";
+import { getAuthCookie } from "../auth";
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const userId = await getAuthCookie(request);
@@ -52,87 +40,15 @@ export async function loader({ request }: LoaderFunctionArgs) {
     } as const);
   }
 
-  const userSubscriptions = await drizzle
-    .select()
-    .from(userDeckSubscriptions)
-    .where(eq(userDeckSubscriptions.userID, userId));
-
   return json({
     allDecks,
     cardQuantity,
     isAuth: true,
-    userSubscriptions,
   } as const);
 }
 
-export const action = async ({ request, params }: ActionFunctionArgs) => {
-  const userId = await requireAuthCookie(request);
-  const formData = await request.formData();
-  const deckIdString = z.coerce.number().parse(formData.get("deckId"));
-  const subscribeString = formData.get("subscribe");
-
-  if (!deckIdString || isNaN(Number(deckIdString))) {
-    return json({ status: "error", error: "Invalid deckId" });
-  }
-
-  const deckId = Number(deckIdString);
-  const isSubscribeAction = formData.has("subscribe");
-
-  const firstCard = await drizzle
-    .select({ cardID: deckCards.cardID })
-    .from(deckCards)
-    .where(eq(deckCards.deckID, deckId))
-    .orderBy(asc(deckCards.cardID))
-    .limit(1);
-
-  try {
-    if (isSubscribeAction) {
-      const subscribe = Boolean(z.coerce.number().parse(subscribeString));
-      const [existingSubscription] = await drizzle
-        .select()
-        .from(userDeckSubscriptions)
-        .where(
-          and(
-            eq(userDeckSubscriptions.deckID, deckId),
-            eq(userDeckSubscriptions.userID, userId)
-          )
-        )
-        .limit(1);
-
-      if (firstCard[0]?.cardID) {
-        await drizzle
-          .insert(userCards)
-          .values({ userID: userId, cardID: firstCard[0]?.cardID })
-          .onConflictDoNothing({
-            target: [userCards.userID, userCards.cardID],
-          });
-      }
-
-      if (existingSubscription) {
-        await drizzle
-          .update(userDeckSubscriptions)
-          .set({ subscribed: subscribe })
-          .where(eq(userDeckSubscriptions.id, existingSubscription.id));
-      } else {
-        await drizzle
-          .insert(userDeckSubscriptions)
-          .values({ userID: userId, deckID: deckId, subscribed: subscribe });
-      }
-      return null;
-    } else if (!isSubscribeAction) {
-      await drizzle.delete(decks).where(eq(decks.id, deckId));
-      return redirect(`/decks`);
-    }
-  } catch (error) {
-    console.log({ deck_delete_error: error });
-    return json({ status: "error" });
-  }
-};
-
 export default function DeckIndex() {
-  const { allDecks, cardQuantity, userSubscriptions, isAuth } =
-    useLoaderData<typeof loader>();
-  const fetcher = useFetcher();
+  const { allDecks, cardQuantity, isAuth } = useLoaderData<typeof loader>();
 
   if (!allDecks) {
     return <div>Decks not found.</div>;
@@ -162,42 +78,20 @@ export default function DeckIndex() {
           const cardsInDeck = cardQuantity?.filter(
             (card) => card?.deckCards.deckID === deck.decks.id
           ).length;
-          const isSubscribed =
-            Number(fetcher.formData?.get("deckId")) === deck.decks.id
-              ? Boolean(fetcher.formData?.get("subscribe"))
-              : userSubscriptions?.find(
-                  (subscription) => subscription.deckID === deck.decks.id
-                )?.subscribed;
           return (
             <div className="deck-box" key={deck.decks.id}>
+              <div className="deck-qty">
+                {cardsInDeck
+                  ? `${cardsInDeck} cards`
+                  : "No cards in this deck."}
+              </div>
               <Link
                 to={`/decks/${deck.decks.id}`}
                 className="deck-text deck-header"
               >
                 {deck.decks.name}
               </Link>
-              <div className="deck-text">
-                {cardsInDeck
-                  ? `${cardsInDeck} cards`
-                  : "No cards in this deck."}
-              </div>
-              <div className="button-container">
-                {isAuth ? (
-                  <fetcher.Form method="POST">
-                    <input type="hidden" name="deckId" value={deck.decks.id} />
-                    <button
-                      aria-label="Toggle Subscription"
-                      className={
-                        isSubscribed ? "unsubscribe-button" : "subscribe-button"
-                      }
-                      name="subscribe"
-                      value={isSubscribed ? 0 : 1}
-                    >
-                      {isSubscribed ? "Unsubscribe" : "Subscribe"}
-                    </button>
-                  </fetcher.Form>
-                ) : null}
-              </div>
+              <div className="button-container"></div>
             </div>
           );
         })}
