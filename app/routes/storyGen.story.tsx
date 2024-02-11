1cc7dd1e44dea583f407e06623bbb05c8690ed89
Jordy <jordynavalee@gmail.com>
2024-02-11 14:44:35 -0600

    styles storyGen form

--- app/routes/storyGen.story.tsx
+++ app/routes/storyGen.story.tsx
@@ -1,18 +1,24 @@
 import { json } from "@remix-run/node";
-import type { ActionFunctionArgs } from "@remix-run/node";
-import { useActionData } from "@remix-run/react";
+import type { LoaderFunctionArgs, ActionFunctionArgs } from "@remix-run/node";
+import { useLoaderData, useActionData } from "@remix-run/react";
 import React from "react";
 import { GenerateStory } from "../../openai-story-generator.mjs";
 import { stories } from "../../db/schema";
 import { drizzle } from "../utils/db.server";
-
-// export async function loader({ request }: LoaderFunctionArgs) {}
+import { eq, and } from "drizzle-orm";
 
 export const action = async ({ request, params }: ActionFunctionArgs) => {
   const formData = await request.formData();
   const storyLength = formData.get("storyLength");
   const genre = formData.get("genre");
   const cefrLevel = formData.get("cefrLevel");
+
+  if (!storyLength || !genre || !cefrLevel) {
+    return json(
+      { error: "Invalid form data, each criteria must be selected" },
+      { status: 400 }
+    );
+  }
 
   interface StoryResponse {
     story: string;
@@ -41,45 +47,87 @@ export const action = async ({ request, params }: ActionFunctionArgs) => {
   }
 
   try {
-    const storyResponseString = await GenerateStory({
-      storyLength,
-      genre,
-      cefrLevel,
-    });
+    const searchStories = await drizzle
+      .select()
+      .from(stories)
+      .where(
+        and(
+          eq(stories.length, storyLength.toString()),
+          eq(
+            stories.genre,
+            genre as
+              | "Mystery"
+              | "Historical Fiction"
+              | "Romance"
+              | "Humor"
+              | "True Crime"
+              | "Adventure"
+              | "Fantasy"
+              | "Folklore"
+              | "Fable"
+              | "Fairy Tale"
+              | "Drama"
+              | "Western"
+              | "Dystopian Fiction"
+              | "Legend"
+              | "Realistic Fiction"
+              | "Tall Tale"
+              | "Biographical"
+              | "Coming-of-age"
+              | "Science Fiction"
+          ),
+          eq(
+            stories.CEFR_level,
+            cefrLevel as "A1" | "A2" | "B1" | "B2" | "C1" | "C2"
+          )
+        )
+      );
 
-    const storyResponse = JSON.parse(
-      storyResponseString ? storyResponseString : "null"
-    ) as StoryResponse;
+    let response;
 
-    const [story] = await drizzle
-      .insert(stories)
-      .values({
-        story: storyResponse.story,
-        length: storyResponse.storyLength,
-        genre: storyResponse.genre,
-        CEFR_level: storyResponse.cefrLevel,
-      })
-      .returning();
+    if (searchStories.length === 0) {
+      const storyResponseString = await GenerateStory({
+        storyLength: storyLength,
+        genre: genre,
+        cefrLevel: cefrLevel,
+      });
 
-    return json({ story: story, error: null } as const);
+      if (storyResponseString) {
+        const storyResponse = JSON.parse(storyResponseString) as StoryResponse;
+
+        const [story] = await drizzle
+          .insert(stories)
+          .values({
+            story: storyResponse.story,
+            length: storyResponse.storyLength,
+            genre: storyResponse.genre,
+            CEFR_level: storyResponse.cefrLevel,
+          })
+          .returning();
+        response = { story: storyResponse, error: null };
+      }
+    } else {
+      response = { story: searchStories, error: null };
+    }
+    return json(response);
   } catch (error) {
     console.error("Error generating story:", error);
-    return json({ story: null, error: "Failed to generate story" } as const, {
+    return json({ data: null, error: "Failed to generate story" } as const, {
       status: 500,
     });
   }
 };
 
 export default function Story() {
-  const storyResponse = useActionData<typeof action>();
-  console.log({ storyResponse });
+  const { story, error } = useActionData<typeof action>() || {};
+
+  console.log({ story });
+
   return (
     <>
-      {storyResponse?.story && (
-        <p className="story-box">{storyResponse?.story.story}</p>
-      )}
+      {story?.story && <p className="story-box">{story?.story}</p>}
 
-      {storyResponse?.error && <p>Failed to generate story</p>}
+      {error && <p>Failed to generate story</p>}
     </>
   );
 }
--- app/routes/storyGen.tsx
+++ app/routes/storyGen.tsx
@@ -1,75 +1,70 @@
-import { LoaderFunctionArgs, ActionFunctionArgs } from "@remix-run/node";
-import { Outlet, useLoaderData, Form } from "@remix-run/react";
+import { Outlet, Form } from "@remix-run/react";
 import React from "react";
-import { eq } from "drizzle-orm";
-import { drizzle } from "../utils/db.server";
-import { stories } from "../../db/schema";
-import { json } from "@remix-run/node";
-
-export async function loader({ request }: LoaderFunctionArgs) {
-  try {
-    const existingStories = await drizzle.select().from(stories);
-    return json(existingStories);
-  } catch (error) {
-    console.log({ story_loader_error: error });
-    return json({ status: "error" });
-  }
-}
 
 export default function StoryGenerator() {
-  const existingStories = useLoaderData<typeof loader>();
-
-  console.log({ existingStories });
-
   return (
     <>
-      <h1>Story Time!</h1>
+      <h1 className="nav-container">Story Time!</h1>
       <Outlet />
       <Form method="post" className="create-form" action={"/storyGen/story"}>
-        <label htmlFor="storyLength">Story Length</label>
-        <select name="storyLength">
-          <option value="">Story Length</option>
-          <option value="short">Short Read</option>
-          <option value="medium">Medium Read</option>
-          <option value="long">Long Read</option>
-        </select>
+        <div className="create-form-section">
+          <label htmlFor="storyLength" className="create-story-label">
+            Story Length
+          </label>
+          <select name="storyLength" className="create-form">
+            <option value="">Story Length</option>
+            <option value="short">Short Read</option>
+            <option value="medium">Medium Read</option>
+            <option value="long">Long Read</option>
+          </select>
+        </div>
 
-        <label htmlFor="genre">Genre</label>
-        <select name="genre">
-          <option value="">Genre</option>
-          <option value="Mystery">Mystery</option>
-          <option value="Historical Fiction">Historical Fiction</option>
-          <option value="Romance">Romance</option>
-          <option value="Humor">Humor</option>
-          <option value="True Crime">True Crime</option>
-          <option value="Adventure">Adventure</option>
-          <option value="Fantasy">Fantasy</option>
-          <option value="Folklore">Folklore</option>
-          <option value="Fable">Fable</option>
-          <option value="Fairy Tale">Fairy Tale</option>
-          <option value="Drama">Drama</option>
-          <option value="Western">Western</option>
-          <option value="Dystopian Fiction">Dystopian Fiction</option>
-          <option value="Legend">Legend</option>
-          <option value="Realistic Fiction">Realistic Fition</option>
-          <option value="Tall Tale">Tall Tale</option>
-          <option value="Biographical">Biographical</option>
-          <option value="Coming-of-age">Coming-of-age</option>
-          <option value="Science Fiction">Science Fiction</option>
-        </select>
+        <div className="create-form-section">
+          <label htmlFor="genre" className="create-story-label">
+            Genre
+          </label>
+          <select name="genre" className="create-form">
+            <option value="">Genre</option>
+            <option value="Mystery">Mystery</option>
+            <option value="Historical Fiction">Historical Fiction</option>
+            <option value="Romance">Romance</option>
+            <option value="Humor">Humor</option>
+            <option value="True Crime">True Crime</option>
+            <option value="Adventure">Adventure</option>
+            <option value="Fantasy">Fantasy</option>
+            <option value="Folklore">Folklore</option>
+            <option value="Fable">Fable</option>
+            <option value="Fairy Tale">Fairy Tale</option>
+            <option value="Drama">Drama</option>
+            <option value="Western">Western</option>
+            <option value="Dystopian Fiction">Dystopian Fiction</option>
+            <option value="Legend">Legend</option>
+            <option value="Realistic Fiction">Realistic Fiction</option>
+            <option value="Tall Tale">Tall Tale</option>
+            <option value="Biographical">Biographical</option>
+            <option value="Coming-of-age">Coming-of-age</option>
+            <option value="Science Fiction">Science Fiction</option>
+          </select>
+        </div>
 
-        <label htmlFor="cefrLevel">Level</label>
-        <select name="cefrLevel">
-          <option value="">CEFR Level</option>
-          <option value="A1">A1</option>
-          <option value="A2">A2</option>
-          <option value="B1">B1</option>
-          <option value="B2">B2</option>
-          <option value="C1">C1</option>
-          <option value="C2">C2</option>
-        </select>
+        <div className="create-form-section">
+          <label htmlFor="cefrLevel" className="create-story-label">
+            Level
+          </label>
+          <select name="cefrLevel" className="create-form">
+            <option value="">CEFR Level</option>
+            <option value="A1">A1</option>
+            <option value="A2">A2</option>
+            <option value="B1">B1</option>
+            <option value="B2">B2</option>
+            <option value="C1">C1</option>
+            <option value="C2">C2</option>
+          </select>
+        </div>
 
-        <button type="submit">Generate Story</button>
+        <button className="button generate-button" type="submit">
+          Generate Story
+        </button>
       </Form>
     </>
   );
